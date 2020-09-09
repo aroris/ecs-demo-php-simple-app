a23a10a84103aae339b145c714e757567c102034
aroris <rishia@gmail.com>
2020-09-09 18:32:37 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Octank Demo PHP App</title>
+        <title>Simple PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -15,31 +15,21 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Octank Demo PHP App</h1>
+                <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p
+                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                 ?>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <!-- Some functions to unit test --> 
-                <?php
-                        class MyClass {
-                            public function method1(int $a, int $b): int {
-                                return $a + $b;
-                            }
 
-                            public function method2(string $a, string $b): string {
-                                return $a . $b;
-                            }
-                        }
-                ?>
             </div>
         </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
     </body>
+
 </html>
